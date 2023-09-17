1baf69e4e979a3f5e4d734df48bb1cb86773dacc
Lucina Lo <lucinalo@Lucinas-Mac-mini.local>
2023-09-17 04:31:20 -0400

    home page and create user page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import './_Home.scss';
 
-const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export const Home = (props) => {
+
+    return (
+        <div className="auth-form-container">
+            <h2>Select Shopper Profile Type</h2>
+            <button type="submit">Search For Lush Member</button>
+            <button onClick={() => props.onFormSwitch('register')}>Create A New User Profile</button>
+            <button type="submit">Continue Shopping As A Guest</button>
+        </div>
+    )
 }
 
 export default Home;
