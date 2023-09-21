470aeeab011ccf05b82e550f495662d744004f1b
Gabriela <lopes.gb18@gmail.com>
2023-09-20 21:56:30 -0400

    page-styling

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import './App.scss';
-import './App.css';
+import "./App.scss";
+import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CreateNewProfile } from "./pages/createNewProfile/CreateNewProfile";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 import { Home } from "./pages/Home/Home";
-import ItemDetailPage from './pages/ItemDetailPage/ItemDetailPage';
-import Test from './pages/Test/Test';
-import Cart from './pages/Cart/Cart';
-import SearchPage from './pages/SearchPage/SearchPage';
-import ProfileList from './pages/ProfileList/ProfileList';
-import { useState } from 'react';
-import CategorizedItems from './pages/CategorizedItems/CategorizedItems';
+import ItemDetailPage from "./pages/ItemDetailPage/ItemDetailPage";
+import Test from "./pages/Test/Test";
+import Cart from "./pages/Cart/Cart";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import ProfileList from "./pages/ProfileList/ProfileList";
+import { useState } from "react";
+import CategorizedItems from "./pages/CategorizedItems/CategorizedItems";
+import AccessibilityPage from "./pages/AccessibilityPage/AccessiblityPage";
 
 function App() {
+  const [userData, setUserData] = useState({});
 
-  const [userData, setUserData] = useState({})
-
-  const [currentForm, setCurrentForm] = useState('home');
+  const [currentForm, setCurrentForm] = useState("home");
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
-  }
+  };
   return (
     <div className="App">
-
       {/* {
         currentForm === "home" ? <Home onFormSwitch={toggleForm} /> : <CreateNewProfile onFormSwitch={toggleForm} />
       } */}
-
-
 
       <BrowserRouter>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path='/item/:id' element={<ItemDetailPage />} />
+            <Route path="/item/:id" element={<ItemDetailPage />} />
             <Route path="/test" element={<Test />} />
             <Route path="/cart" element={<Cart />} />
             <Route
-              path='/search'
+              path="/search"
               element={<SearchPage setUserData={setUserData} />}
-
             />
             <Route
-              path='/profile-list'
+              path="/profile-list"
               element={<ProfileList userData={userData} />}
-            <Route path="/category/:category" element={<CategorizedItems />} />
             />
+            <Route path="/category/:category" element={<CategorizedItems />} />
+            <Route path="/accessibility" element={<AccessibilityPage />} />
           </Routes>
         </main>
         <Navbar />
